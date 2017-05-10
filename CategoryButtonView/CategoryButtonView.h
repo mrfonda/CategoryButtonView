06f1e8d9db65da296dba0aeb3e95d104d97c316e
Vlad <vladislav.dorfman@gmail.com>
2017-05-10 15:52:17 +0300

    Initial Commit

---
+++ CategoryButtonView/CategoryButtonView.h
@@ -0,0 +1,19 @@
+//
+//  CategoryButtonView.h
+//  CategoryButtonView
+//
+//  Created by Vladislav Dorfman on 10/05/2017.
+//  Copyright © 2017 Vladislav Dorfman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CategoryButtonView.
+FOUNDATION_EXPORT double CategoryButtonViewVersionNumber;
+
+//! Project version string for CategoryButtonView.
+FOUNDATION_EXPORT const unsigned char CategoryButtonViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CategoryButtonView/PublicHeader.h>
+
+
